eaa783c7403442099241210302558c2a86adc490
Nick Amin <amin.nj@gmail.com>
2017-01-21 16:46:39 -0800

    allow basepath specification to reduce clutter, allow fake files, and force user to re-compute existence boolean

--- File.py
+++ File.py
@@ -5,21 +5,46 @@ from Constants import Constants
 import Utils
 
 class File(object):
-    def __init__(self, name, status=None):
+    def __init__(self, name, status=None, fake=False, basepath=None):
         self.file_exists = None
+        self.basepath = None
         self.status = status
         self.name = name
 
+        if basepath:
+            self.name = os.path.join(basepath,name)
+
+        if fake:
+            self.file_exists = True
+            self.status = Constants.FAKE
+
+    def __repr__(self):
+        info = "name={},status={}".format(self.name,self.status)
+        return "{}({})".format(self.__class__.__name__,info)
+
+    def get_name(self):
+        return self.name
+
     def exists(self):
+        """
+        Important NOTE:
+        Below if statement basically caches the existence of
+        this file. Call the update() method to re-check.
+        """
         if self.file_exists is None:
             self.file_exists = os.path.exists(self.name)
         return self.file_exists
+    
+    def update(self):
+        self.file_exists = os.path.exists(self.name)
 
     def set_status(self, status):
+        self.update()
         self.status = status
 
     def get_status(self):
         return self.status
+
 
 class EventsFile(File):
     def __init__ (self, name, status=None, nevents=None, nevents_negative=None):
@@ -30,11 +55,4 @@ class EventsFile(File):
         self.nevents_negative = nevents_negative
 
 if __name__ == '__main__':
-    f = File(__file__)
-    assert(f.exists())
-
-    ef = EventsFile("does_not_exist.root", status=Constants.VALID, nevents=123)
-    assert(ef.get_status() == Constants.VALID)
-    ef.set_status(Constants.INVALID)
-    assert(ef.get_status() == Constants.INVALID)
-    assert(not ef.exists())
+    pass
