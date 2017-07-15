8e6580b482c214b880ad71e354f65a7c8a553231
Nick Amin <amin.nj@gmail.com>
2017-07-14 18:29:30 -0700

    add script to make some mc from LHE to miniaod

---
+++ makemc.py
@@ -0,0 +1,52 @@
+import Utils
+from pprint import pprint
+from CMSSWTaskSmall import CMSSWTask
+from Sample import DirectorySample
+
+lhe = CMSSWTask(
+        sample = DirectorySample(
+            location="/hadoop/cms/store/user/namin/lhe_Apr1/",
+            globber="*seed6*.lhe",
+            dataset="/stop-stop/procv1/LHE",
+            ),
+        events_per_output = 20,
+        total_nevents = 100,
+        pset = "mcmaking/pset_gensim.py",
+        cmssw_version = "CMSSW_7_1_20_patch3",
+        split_within_files = True,
+        )
+
+raw = CMSSWTask(
+        sample = DirectorySample(
+            location = lhe.get_outputdir(),
+            dataset = lhe.get_sample().get_datasetname().replace("LHE","RAW"),
+            ),
+        files_per_output = 1,
+        pset = "mcmaking/pset_raw.py",
+        cmssw_version = "CMSSW_8_0_21",
+        )
+
+aod = CMSSWTask(
+        sample = DirectorySample(
+            location = raw.get_outputdir(),
+            dataset = raw.get_sample().get_datasetname().replace("RAW","AOD"),
+            ),
+        files_per_output = 1,
+        pset = "mcmaking/pset_aod.py",
+        cmssw_version = "CMSSW_8_0_21",
+        )
+
+miniaod = CMSSWTask(
+        sample = DirectorySample(
+            location = aod.get_outputdir(),
+            dataset = aod.get_sample().get_datasetname().replace("AOD","MINIAOD"),
+            ),
+        files_per_output = 2,
+        pset = "mcmaking/pset_miniaod.py",
+        cmssw_version = "CMSSW_8_0_21",
+        )
+
+workflow = [lhe, raw, aod, miniaod]
+for part in workflow:
+    part.process()
+
