cef3939535fc5236904e7d6a6e0060bcdf9bee90
Nick Amin <amin.nj@gmail.com>
2017-07-30 17:01:27 -0500

    :scissors: clean up

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source /code/osgcode/cmssoft/cms/cmsset_default.sh
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_8_0_20/; cmsenv; cd -
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc493/cms/cmssw/CMSSW_8_0_20/ && cmsenv && cd -
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 
-export METIS_BASE=`pwd`
-cd $METIS_BASE
+# export METIS_BASE=`pwd`
+# cd $METIS_BASE
 
 # CRAB screws up our PYTHONPATH. Go figure.
 export PYTHONPATH=$(pwd):$PYTHONPATH
